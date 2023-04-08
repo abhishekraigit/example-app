ee9b37a25df1366925942a72649300fb2e0dc084
abhishekraigit <abhishek@ekanatechnologies.in>
2023-04-08 11:50:34 +0530

    add card

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="conatiner">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>hello abhishek rai</h1>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, laboriosam error blanditiis expedita atque maiores incidunt! Quia corporis quam aspernatur incidunt dolores facilis, veritatis voluptates quod! Quibusdam porro cumque, qui voluptas quaerat necessitatibus corrupti amet, deserunt voluptate excepturi sunt voluptates sequi saepe repellendus, eos harum.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <div class="card-header">
+                        <h1>hello Zaid </h1>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, laboriosam error blanditiis expedita atque maiores incidunt! Quia corporis quam aspernatur incidunt dolores facilis, veritatis voluptates quod! Quibusdam porro cumque, qui voluptas quaerat necessitatibus corrupti amet, deserunt voluptate excepturi sunt voluptates sequi saepe repellendus, eos harum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
